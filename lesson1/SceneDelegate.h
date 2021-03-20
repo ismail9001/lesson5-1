462726c62c865a6cc95abd67f6a2ff29d550ed3f
Iskander Nizamov <nizamov-iskander@yandex.ru>
2021-03-20 10:51:38 +0300

    Initial Commit

---
+++ lesson1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  lesson1
+//
+//  Created by macbook on 20.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ lesson1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson1
+//
+//  Created by macbook on 20.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
